8ba1f6ca9215cce6bfa2b669a18967667d6a973a
LudvikBar <ludvik.barlaug@outlook.com>
2025-10-24 09:12:26 +0200

    first commit

---
+++ firstgame/scripts/player_1.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+
+const SPEED = 160.0
+const JUMP_VELOCITY = -400.0
+var coyote_time := 0.1  # Seconds you can still jump after leaving ground
+var coyote_timer := 0.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+
+func _physics_process(delta: float) -> void:
+	# Track coyote timer
+	if is_on_floor():
+		coyote_timer = coyote_time  # Reset timer when on ground
+	else:
+		coyote_timer -= delta  # Count down when in air
+
+	# Add gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump (allow jump while timer > 0)
+	if Input.is_action_just_pressed("ui_accept") and coyote_timer > 0:
+		velocity.y = JUMP_VELOCITY
+		coyote_timer = 0  # Prevent double-jumping
+
+	# Movement
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		
+		
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("run")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
